562fe21799500622b848764e48060016af92efbc
Andri <andri@ThomasVirgins-Mac-mini.local>
2018-02-09 11:27:50 +0200

    universal

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable()
 export class ApiService {
